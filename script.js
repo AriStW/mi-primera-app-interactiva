ecb9beb3829551752bae7e54294736cb5f1de4f4
AriStWeb <sole21008@gmail.com>
2025-06-21 18:43:52 -0300

    create js functions and modify index and some style

---
+++ script.js
@@ -0,0 +1,48 @@
+function initGame() {
+    const welcome = document.getElementById("welcome");
+    const questions = document.getElementById("questions");
+
+    let name = prompt("Por favor, ingresa tu nombre:");
+    welcome.textContent = "Hola " + name + " las Reglas son";
+    questions.style.display = "flex";
+}
+
+function answerYes(question, buttonYes, buttonNo) {
+
+    question.style.backgroundColor = "#00FF00";
+    buttonYes.style.display = "none";
+    buttonNo.style.display = "block";
+}
+
+function answerNo(question, buttonNo, buttonYes) {
+
+    question.style.backgroundColor = "#FF0000";
+    buttonNo.style.display = "none";
+    buttonYes.style.display = "block";
+}
+
+function seeResult() {
+    let result = prompt("Por favor, ingresa la cantidad de respuestas en verde:");
+    printResult(result);
+    console.log(result);
+}
+console.log("hola")
+
+function printResult(result) {
+    const result8to10 = document.getElementById("result8to10");
+    const result5to7 = document.getElementById("result5to7");
+    const result0to4 = document.getElementById("result0to4");
+    const finishResult = document.getElementById("listResult");
+
+    finishResult.style.display = "flex";
+
+    if (result <= 4) {
+        result0to4.style.backgroundColor = "#00FF00";
+    }
+    else if (result >= 8) {
+        result8to10.style.backgroundColor = "#00FF00";
+    }
+    else if (result >= 5 && result <= 7) {
+        result5to7.style.backgroundColor = "#00FF00";
+    }
+}
